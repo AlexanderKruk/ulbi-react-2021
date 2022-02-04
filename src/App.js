175e801003329f1e6b6cb7ef7780ba2aa35460ea
Alexander Kruk <alexander.kruk@gmail.com>
2022-02-04 16:34:31 +0100

    added state and controlled component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [num, setNum] = useState(0);
+  const [value, setValue] = useState("");
+
+  const inc = () => {
+    setNum(num + 1);
+  };
+
+  const dec = () => {
+    setNum(num - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{num}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
     </div>
   );
 }
