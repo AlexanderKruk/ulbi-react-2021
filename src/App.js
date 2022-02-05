56e45cfe1fe1b5f5c6976b49b54b116951fb5654
Alexander Kruk <alexander.kruk@gmail.com>
2022-02-05 13:08:05 +0100

    add component with props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
+import { PostItem } from "./components/PostItem";
 import "./styles/App.css";
 
 function App() {
   return (
     <div className="app">
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>JS programmin language</div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
-      </div>
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>JS programmin language</div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
-      </div>
-      <div className="post">
-        <div className="post__content">
-          <strong>1. Javascript</strong>
-          <div>JS programmin language</div>
-        </div>
-        <div className="post__btns">
-          <button>Delete</button>
-        </div>
-      </div>
+      <PostItem
+        post={{
+          id: 1,
+          title: "Javascript",
+          description: "JS programmin language",
+        }}
+      />
+      <PostItem
+        post={{
+          id: 2,
+          title: "Javascript",
+          description: "JS programmin language",
+        }}
+      />
+      <PostItem
+        post={{
+          id: 3,
+          title: "Javascript",
+          description: "JS programmin language",
+        }}
+      />
     </div>
   );
 }
