c95d1359cb42f53ac2e8b2ccf30f590d440688a6
Alexander Kruk <alexander.kruk@gmail.com>
2022-02-05 16:12:42 +0100

    added PostList

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import { PostItem } from "./components/PostItem";
+import { useState } from "react";
+import { PostList } from "./components/PostList";
 import "./styles/App.css";
 
 function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", description: "JS programmin language" },
+    { id: 2, title: "Javascript", description: "JS programmin language" },
+    { id: 3, title: "Javascript", description: "JS programmin language" },
+  ]);
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: "Python", description: "JS programmin language" },
+    { id: 2, title: "Python", description: "JS programmin language" },
+    { id: 3, title: "Python", description: "JS programmin language" },
+  ]);
+
   return (
     <div className="app">
-      <PostItem
-        post={{
-          id: 1,
-          title: "Javascript",
-          description: "JS programmin language",
-        }}
-      />
-      <PostItem
-        post={{
-          id: 2,
-          title: "Javascript",
-          description: "JS programmin language",
-        }}
-      />
-      <PostItem
-        post={{
-          id: 3,
-          title: "Javascript",
-          description: "JS programmin language",
-        }}
-      />
+      <PostList posts={posts} title={"List of posts"} />
+      <PostList posts={posts2} title={"List of posts"} />
     </div>
   );
 }
