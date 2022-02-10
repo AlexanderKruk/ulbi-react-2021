ed572a0ad14c3ddf16471e3b50a9b148ec6a3daa
Alexander Kruk <alexander.kruk@gmail.com>
2022-02-10 11:30:04 +0100

    changed index in PostList

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { PostItem } from './PostItem';
 
-export const PostList = ({posts, title}) => {
+export const PostList = ({posts, title ,deletePost}) => {
   return(
     <>
       <h1 style={{ textAlign: "center" }}>{title}</h1>
-      {posts.map((post) => (
-        <PostItem key={post.id} post={post} />
+      {posts.map((post, index) => (
+        <PostItem key={post.id} post={post} index={index} deletePost={deletePost} />
       ))}
     </>
   );
