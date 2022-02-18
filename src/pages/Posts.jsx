df8046d268b0b48b174d6d7533b26d606ebe20d0
Alexander Kruk <alexander.kruk@gmail.com>
2022-02-18 15:38:14 +0100

    add limit selector

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { PostForm } from "../components/PostForm";
 import { PostList } from "../components/PostList";
 import "../styles/App.css";
@@ -10,22 +10,24 @@ import { PostService } from "../api/PostService";
 import { Loader } from "../components/ui/Loader/Loader";
 import { useFetching } from "../hooks/useFetching";
 import { getPageCount } from "../utils/pages";
-import { Pagination } from "../components/ui/Pagination/Pagination";
+import { useObserver } from '../hooks/useObserver'
+import { Select } from "../components/ui/Select/Select";
 
 export const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [visibleModal, setVisibleModal] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
-  const [limit ] = useState(10);
+  const [limit, setLimit ] = useState(10);
   const [page, setPage] = useState(1);
+  const lastElement = useRef()
 
   const [postsFetch, isLoading, error] = useFetching(async () => {
     const response = await PostService.getAll(page, limit);
     if (response) {
       const totalCount = response.headers["x-total-count"];
       setTotalPages(getPageCount(totalCount, limit));
-      setPosts(response.data);
+      setPosts([...posts, ...response.data]);
     }
   });
 
@@ -33,7 +35,11 @@ export const Posts = () => {
 
   useEffect(() => {
     postsFetch();
-  }, [page]);
+  }, [page, limit]);
+
+  useObserver(lastElement, page < totalPages, isLoading,() => {
+    setPage(page + 1)
+  })
 
   const addPost = (e, post) => {
     e.preventDefault();
@@ -55,12 +61,23 @@ export const Posts = () => {
       </Modal>
       <hr style={{ margin: "20px 0px" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
+      <Select
+        selectedValue={limit}
+        onChange={(e) => setLimit(e.target.value)}
+        defaultValue={"Count of posts"}
+        options={[
+          {value: 5, name: "5"},
+          {value: 10, name: "10"},
+          {value: -1, name: "all"}
+        ]}
+      />
       <PostList
           posts={sortedAndSearchedPosts}
           title={"List of posts"}
           deletePost={deletePost}
           error={error}
         />
+      <div ref={lastElement} style={{height: 20, background: 'red'}}></div>
       {isLoading && (
         <div
           style={{
@@ -71,7 +88,6 @@ export const Posts = () => {
           <Loader />
         </div>
       )}
-      <Pagination totalPages={totalPages} page={page} setPage={setPage} />
     </div>
   );
 }
